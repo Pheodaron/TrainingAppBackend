38bf403398722b47449e9cfd3b2b7ad9b6b1197a
Pheodaron <grindar10101@gmail.com>
2022-04-28 13:52:44 +0300

    first initialization

---
+++ src/main/java/com/pheodaron/TrainingApp/TrainingAppApplication.java
@@ -0,0 +1,13 @@
+package com.pheodaron.TrainingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pheodaron/TrainingApp/TrainingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pheodaron.TrainingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
